f67c9d154ce7e25685be0e9e22465859cab7f4a3
Kafva <Martensson.Jonas98@gmail.com>
2022-02-11 10:25:48 +0100

    -

--- toys.mk
+++ toys.mk
@@ -6,6 +6,24 @@ CFLAGS=-DCBMC=false
 .PHONY: smt clean run bmc diff
 
 
+# The desired interface:
+# 	./euf --dep ../oniguruma --commit-current <...> --commit-new <...> ../jq
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #---- Bounded Model Checker ----#
 # CBMC is meant to assess if an assertion is true
 # for all possible executions of a program
@@ -39,7 +57,7 @@ analyze:
 	clang -I toy/include $(CFLAGS) -analyze -analyzer-checker=core.DivideZero toy/src/*
 
 diff:
-	mkdir -p ir
+	mkdir -p toy/ir
 
 	clang -I toy/include -S -emit-llvm toy/src/$(DEP).c -o toy/ir/$(DEP).old.ll
 
