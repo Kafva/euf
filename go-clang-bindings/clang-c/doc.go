79dde1f79c7246e6f1c36391c6ca2e59d8e36709
Kafva <Martensson.Jonas98@gmail.com>
2022-02-21 15:58:59 +0100

    Removed Go version

--- go-clang-bindings/clang-c/doc.go
+++
@@ -1,2 +0,0 @@
-// Package clang_c holds clang binding C header files.
-package clang_c
--- lib/config.go
+++
@@ -1,8 +0,0 @@
-package lib
-
-const HELP_STR = ""
-
-var DEBUG *bool
-var DEPENDENCY_DIR  *string 	
-var OLD_COMMIT      *string 	
-var NEW_COMMIT 	    *string
