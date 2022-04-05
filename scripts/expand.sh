8a61bf945135c52a20d2a7888efdc5f66c4aef27
Kafva <Martensson.Jonas98@gmail.com>
2022-04-05 16:25:35 +0200

    -

--- scripts/expand.sh
+++ scripts/expand.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 while read -r line; do
 
-  if $(echo $line|grep -q ".[ch]$"); then
-      echo $line
+  if $(echo $line|grep -q "\.[ch]$"); then
+      clang -Ilib -E $line -o $line
   fi
 
-done <(git ls-tree -r HEAD --name-only)
+done < <(git ls-tree -r HEAD --name-only)
--- scripts/multi_diff.sh
+++ scripts/multi_diff.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 die(){ echo -e "$1" >&2 ; exit 1; }
 
-START=3
+START=10
 DIRCNT=25
 OUTFILE=/tmp/expat_diffs
 TMPOUT=$(mktemp)
-EXCLUDE=(
-	libexpat-bbdfcfef  libexpat-c16300f0  libexpat-e07e3947
-)
 
 cd ~/.cache/expats
 rm -f $OUTFILE
@@ -33,9 +30,11 @@ while read -r line; do
 
 	for i in $(seq $START $DIRCNT); do
 		for j in $(seq $i $DIRCNT); do
-			[ $i = $j ] && continue
-			printf "\033[34m===============$i $j=================\033[0m\n"
-			git --no-pager diff --color=always --no-index expat$i/$name expat$j/$name
+      if [[ -d expat$i && -d expat$j ]]; then
+        [ $i = $j ] && continue
+        printf "\033[34m===============$i $j=================\033[0m\n"
+        git --no-pager diff --color=always --no-index expat$i/$name expat$j/$name
+      fi
 		done
 	done
 
