4a7b24ddfffaf21d65dd1f5b4a1d49ff7d77b8a2
Kafva <Martensson.Jonas98@gmail.com>
2022-04-30 11:54:26 +0200

    -

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -18,12 +18,6 @@ image_exists "euf-base" ||
 image_exists "euf" ||
   docker build --rm --tag=euf .
 
-# Run with source files mounted to enable a more agile dev flow
-# NOTE: Files edited in vim use a swap file which is copied into place
-# on save, since bind mounts are based on inode numbers we will
-# still see the old file in the container. Because of this we cannot
-# live-reload file-mounts, only directory mounts and instead
-# run a background sync job
 while :; do
   sleep 2
   docker cp euf.py $(docker-id euf):/home/euf/euf/euf.py
@@ -31,10 +25,16 @@ done &
 
 SYNC_PID=$!
 
+# This invocation runs with source files mounted to enable a more agile dev flow
+# NOTE: Files edited in vim use a swap file which is copied into place
+# on save, since bind mounts are based on inode numbers we will
+# still see the old file in the container. Because of this we cannot
+# live-reload file-mounts, only directory mounts and instead
+# run a background sync job for the euf.py script
 docker run -h euf -it \
   -u euf:root \
-  -v /home/jonas/Repos/.docker/jq:/home/euf/Repos/jq \
-  -v /home/jonas/Repos/.docker/oniguruma:/home/euf/Repos/oniguruma \
+  -v $HOME/Repos/.docker/jq:/home/euf/Repos/jq \
+  -v $HOME/Repos/.docker/oniguruma:/home/euf/Repos/oniguruma \
   -v $PWD/tests/configs:/home/euf/euf/tests/configs \
   -v $PWD/scripts:/home/euf/euf/scripts \
   -v $PWD/src:/home/euf/euf/src \
