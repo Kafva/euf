c7508015684660e0741ff60677b26f86a8ea888c
Kafva <Martensson.Jonas98@gmail.com>
2022-05-06 09:43:48 +0200

    Refactor visualise scripts

--- visualise/main.py
+++ visualise/main.py
@@ -24,7 +24,7 @@
 
   Plots
   ======
-  * How many of each type of results were derived from each analysis?
+  * How are the Analysis Results distributed?
       * Plotting the mean for each type of result from each case could be somewhat
         interesting as well
   * Reduction in change and impact set (mean,lowest,highest graph)
@@ -32,7 +32,7 @@
 
 
 '''
-import sys, os
+import sys
 import matplotlib.pyplot as plt
 from itertools import compress
 from textwrap import wrap
@@ -40,135 +40,16 @@ from textwrap import wrap
 # '.' is needed to run from the visualise directory 
 sys.path.extend(['..','.'])
 
-from visualise.types import CbmcResult, FunctionResult
+from visualise.types import Case
 from src.config import CONFIG
 from src.types import AnalysisResult
-from src.util import print_info, print_stage
 
-def get_results_from_csv(name:str) -> \
- tuple[dict[str,FunctionResult],dict[str,list[CbmcResult]]]:
-    function_results = {}
-    cbmc_results = {}
-
-    for item in os.listdir(CONFIG.RESULTS_DIR):
-        dirpath = f"{CONFIG.RESULTS_DIR}/{item}"
-        cbmc_results[dirpath] = []
-
-        if os.path.isdir(dirpath) and item.startswith(name):
-            if os.path.isfile(f"{dirpath}/cbmc.csv"):
-                with open(f"{dirpath}/cbmc.csv", mode = 'r', encoding='utf8') as f:
-                    for line in f.readlines()[1:]:
-                        cbmc_results[dirpath].append(
-                            CbmcResult.new(line.split(";"), dirpath)
-                        )
-
-                        # Add a key for each function name
-                        func_name = cbmc_results[dirpath][-1].func_name
-                        if func_name not in function_results:
-                            function_results[func_name] =\
-                            FunctionResult(func_name=func_name)
-
-        # Join entries with for the same function into one
-        for func_name,func_result in function_results.items():
-            func_result.results.extend(
-                map(lambda a: a.result, filter(lambda c:
-                    not c.identity and c.func_name == func_name,
-                    cbmc_results[dirpath]
-                )
-            ))
-            func_result.results_id.extend(
-                map(lambda a: a.result, filter(lambda c:
-                    c.identity and c.func_name == func_name,
-                    cbmc_results[dirpath]
-                )
-            ))
-
-    return function_results, cbmc_results
-
-def correctness_per_function(name:str,
- function_results: dict[str,FunctionResult], ident:bool=False):
-    msg = name + (" (identity)" if ident else '')
-    print_stage(msg)
-
-    if ident:
-        successes = list(filter(lambda v:
-                AnalysisResult.SUCCESS in v.results_id or
-                AnalysisResult.SUCCESS_UNWIND_FAIL in v.results_id,
-                function_results.values()
-        ))
-        failures = list(filter(lambda v:
-                AnalysisResult.FAILURE in v.results_id or
-                AnalysisResult.FAILURE_UNWIND_FAIL in v.results_id,
-                function_results.values()
-        ))
-        errors = list(filter(lambda v:
-                AnalysisResult.ERROR in v.results or
-                AnalysisResult.STRUCT_CNT_CONFLICT in v.results_id or
-                AnalysisResult.STRUCT_TYPE_CONFLICT in v.results_id,
-                function_results.values()
-        ))
-    else:
-        successes = list(filter(lambda v:
-                AnalysisResult.SUCCESS in v.results or
-                AnalysisResult.SUCCESS_UNWIND_FAIL in v.results,
-                function_results.values()
-        ))
-        failures = list(filter(lambda v:
-                AnalysisResult.FAILURE in v.results or
-                AnalysisResult.FAILURE_UNWIND_FAIL in v.results,
-                function_results.values()
-        ))
-        errors = list(filter(lambda v:
-                AnalysisResult.ERROR in v.results or
-                AnalysisResult.STRUCT_CNT_CONFLICT in v.results or
-                AnalysisResult.STRUCT_TYPE_CONFLICT in v.results,
-                function_results.values()
-        ))
-
-
-    if DUMP_SUCCESS:
-        for s in successes: print(s.pretty(ident=ident))
-    print_info(f"Successes: {len(successes)}")
-    print_info(f"Failures: {len(failures)}")
-    print_info(f"Errors: {len(errors)}")
-
-def get_result_distribution(cbmc_results: dict[str,list[CbmcResult]],
- ident:bool=False) -> tuple[list[str],list[int]]:
-    '''
-    Create a plot showing the distribution of results from each item
-    in the provided cbmc_results list
-    '''
-    result_cnts = { e.name: 0 for e in AnalysisResult }
-    for cbmc_list in cbmc_results.values():
-        filtered = filter(lambda x: x.identity == ident, cbmc_list)
-        for c in filtered:
-            result_cnts[c.result.name] += 1
-
-    # Combine _unwind cases
-    result_cnts[AnalysisResult.SUCCESS.name] += \
-        result_cnts[AnalysisResult.SUCCESS_UNWIND_FAIL.name]
-    result_cnts[AnalysisResult.FAILURE.name] += \
-        result_cnts[AnalysisResult.FAILURE_UNWIND_FAIL.name]
-    del result_cnts[AnalysisResult.SUCCESS_UNWIND_FAIL.name]
-    del result_cnts[AnalysisResult.FAILURE_UNWIND_FAIL.name]
-
-    # Exclude results that never occured in any of the cases
-    #result_cnts = { key: val for key,val in result_cnts.items() if val != 0 }
-
-    bar_names = list(result_cnts.keys())
-
-    # Wrap the bar name text to X chars
-    bar_names = [ '\n'.join(wrap(l, 10)) for l in bar_names ]
-
-    bar_values = list(result_cnts.values())
-    return bar_names, bar_values
-
-def write_md():
+def write_md(cases: list[Case]):
     ''' Make a MD template for the correctness analysis '''
     with open("correctness.md", mode = 'w', encoding='utf8') as f:
-        for i,test_case_result in enumerate(TEST_CASE_RESULTS):
-            f.write(f"# {TEST_LIBS[i]}\n")
-            for _, func_result in test_case_result.items():
+        for case in cases:
+            f.write(f"# {case.name}\n")
+            for func_result in case.function_results():
                 overlap = set(func_result.results) & {AnalysisResult.SUCCESS,
                    AnalysisResult.SUCCESS_UNWIND_FAIL,
                    AnalysisResult.SUCCESS_UNWIND_FAIL,
@@ -179,81 +60,67 @@ def write_md():
                     # was present for the current function
                     # and print an analysis command for each one
                     f.write("```bash\n")
-                    for cbmc_result in TEST_CASE_CBMCS[i].values():
-                        for r in cbmc_result:
-                            if r.func_name == func_result.func_name and not r.identity:
-                                f.write(f"./scripts/analyze_function.sh "
-                                    f"{TEST_LIBS[i].removeprefix('lib')} "
-                                    f"{r.func_name} "
-                                    f"{r.commit_old} {r.commit_new}\n"
-                                )
-                                f.write(f"# => {r.driver}\n")
+                    for r in case.cbmc_results():
+                        if r.func_name == func_result.func_name and not r.identity:
+                            f.write(f"./scripts/analyze_function.sh "
+                                f"{case.name.removeprefix('lib')} "
+                                f"{r.func_name} "
+                                f"{r.commit_old} {r.commit_new}\n"
+                            )
+                            f.write(f"# => {r.driver}\n")
                     f.write("```\n")
                     f.write("\n\n")
 
-def result_dists(bar_names,onig_cnts,expat_cnts,usb_cnts,ident:bool=False):
-    '''
-    Exclude AnalysisResult cases which had zero occurrences 
-    for all cases
-    '''
-    non_zero_fields = [ a!=0 or b!=0 or c!=0 for a,b,c in
-            zip(onig_cnts,expat_cnts,usb_cnts) ]
+def plot_analysis_dists(cases: list[Case],ident:bool=False):
+    cases_dists = [ c.analysis_dist(ident=ident).values() for c in cases ]
 
+    non_zero_fields = [ a!=0 or b!=0 or c!=0 for a,b,c in
+            zip(cases_dists[0],
+                cases_dists[1],
+                cases_dists[2]) ]
+
+    # Remove fields that are zero across all cases
+    cases_dists = [ list(compress(c,non_zero_fields)) for c in cases_dists ]
+
+
+    bar_names = [ e.name for e in AnalysisResult ]
     bar_names  = list(compress(bar_names, non_zero_fields))
-    onig_cnts  = list(compress(onig_cnts, non_zero_fields))
-    expat_cnts = list(compress(expat_cnts, non_zero_fields))
-    usb_cnts   = list(compress(usb_cnts, non_zero_fields))
+
+    # Wrap the bar name text to X chars
+    bar_names = [ '\n'.join(wrap(l, 10)) for l in bar_names ]
 
     _, axes = plt.subplots()
     width = 0.35
 
     # Color-code a bar plot for each case
-    axes.bar(bar_names, onig_cnts,  width,  label='libonig')
-    axes.bar(bar_names, expat_cnts, width,  label='libexpat')
-    axes.bar(bar_names, usb_cnts,   width,  label='libusb')
+    for i,case in enumerate(cases):
+        axes.bar(bar_names, cases_dists[i],  width,  label=case.name)
 
-    axes.set_ylabel('Occurrences')
+    axes.set_ylabel('')
     axes.set_title(f"Distribution of CBMC {'identity ' if ident else ''}analysis results")
     axes.legend()
-
 
     plt.xticks(fontsize=10)
     plt.show()
 
 if __name__ == '__main__':
     WRITE_MD = True
-    PLOT = False
-    DUMP_SUCCESS = True
-    CONFIG.RESULTS_DIR = ".results/3"
+    PLOT = True
+    CONFIG.RESULTS_DIR = ".results/5"
 
-    onig_results, ONIG_CBMC = get_results_from_csv("libonig")
-    correctness_per_function("libonig", onig_results, ident=False)
-    correctness_per_function("libonig", onig_results, ident=True)
+    onig = Case.new(name="libonig", total_functions=1186)
+    onig.info()
+    expat = Case.new(name="libexpat", total_functions=645)
+    expat.info()
+    usb = Case.new(name="libusb", total_functions=1346)
+    usb.info()
 
-    expat_results, EXPAT_CBMC = get_results_from_csv("libexpat")
-    correctness_per_function("libexpat", expat_results, ident=False)
-    correctness_per_function("libexpat", expat_results, ident=True)
+    cases = [onig,expat,usb]
 
-    usb_results, USB_CBMC = get_results_from_csv("libusb")
-    correctness_per_function("libusb", usb_results, ident=False)
-    correctness_per_function("libusb", usb_results, ident=True)
-
-    TEST_LIBS = ["libonig", "libexpat", "libusb"]
-    TEST_CASE_RESULTS = [onig_results, expat_results, usb_results]
-    TEST_CASE_CBMCS = [ONIG_CBMC, EXPAT_CBMC, USB_CBMC]
-
-    if WRITE_MD:
-        write_md()
     if PLOT:
-        bar_names, onig_cnts = get_result_distribution(ONIG_CBMC)
-        _, expat_cnts = get_result_distribution(EXPAT_CBMC)
-        _, usb_cnts = get_result_distribution(USB_CBMC)
+        plot_analysis_dists(cases,ident=True)
+        plot_analysis_dists(cases,ident=False)
+    if WRITE_MD:
+        write_md(cases)
 
-        result_dists(bar_names,onig_cnts,expat_cnts,usb_cnts)
-
-        bar_names, onig_cnts = get_result_distribution(ONIG_CBMC,ident=True)
-        _, expat_cnts = get_result_distribution(EXPAT_CBMC,ident=True)
-        _, usb_cnts = get_result_distribution(USB_CBMC,ident=True)
-
-        result_dists(bar_names,onig_cnts,expat_cnts,usb_cnts,ident=True)
 
--- visualise/types.py
+++ visualise/types.py
@@ -1,8 +1,10 @@
-import re
+import os,re
+from pprint import pprint
 from dataclasses import dataclass, field
 from datetime import datetime
 from src.config import CONFIG
 from src.types import AnalysisResult, IdentifierLocation
+from src.util import flatten, print_stage
 
 @dataclass(init=True)
 class CbmcResult:
@@ -73,4 +75,167 @@ class FunctionResult:
             cnt = res.count(r)
             out += f"> {r.name} ({cnt})\n"
         return out
+
+@dataclass(init=True)
+class Case:
+    '''
+    Holds descriptive data for each case: libonig, libexpat and libusb
+
+
+    '''
+    total_functions: int
+    name: str
+
+    # Holds one entry per function that was analyzed (dict key),
+    # each item contains an array of AnalysisResult that
+    # were encountered for the particular function
+    # during full and identity analysis
+    function_results_dict: dict[str,FunctionResult]
+
+    def function_results(self) -> list[FunctionResult]:
+        return [ r for r in self.function_results_dict.values() ]
+
+    # Holds one entry per CSV row across all results, dict key
+    # is the path to the CSV file
+    cbmc_results_dict: dict[str,list[CbmcResult]]
+
+    def cbmc_results(self) -> list[CbmcResult]:
+        return flatten([ r for r in self.cbmc_results_dict.values() ])
+
+    @classmethod
+    def load_from_csv_files(cls,name:str) -> \
+     tuple[dict[str,FunctionResult],dict[str,list[CbmcResult]]]:
+        function_results = {}
+        cbmc_results = {}
+
+        for item in os.listdir(CONFIG.RESULTS_DIR):
+            dirpath = f"{CONFIG.RESULTS_DIR}/{item}"
+            cbmc_results[dirpath] = []
+
+            if os.path.isdir(dirpath) and item.startswith(name):
+                if os.path.isfile(f"{dirpath}/cbmc.csv"):
+                    with open(f"{dirpath}/cbmc.csv", mode = 'r', encoding='utf8') as f:
+                        for line in f.readlines()[1:]:
+                            cbmc_results[dirpath].append(
+                                CbmcResult.new(line.split(";"), dirpath)
+                            )
+
+                            # Add a key for each function name
+                            func_name = cbmc_results[dirpath][-1].func_name
+                            if func_name not in function_results:
+                                function_results[func_name] =\
+                                FunctionResult(func_name=func_name)
+
+            # Join entries with for the same function into one
+            for func_name,func_result in function_results.items():
+                func_result.results.extend(
+                    map(lambda a: a.result, filter(lambda c:
+                        not c.identity and c.func_name == func_name,
+                        cbmc_results[dirpath]
+                    )
+                ))
+                func_result.results_id.extend(
+                    map(lambda a: a.result, filter(lambda c:
+                        c.identity and c.func_name == func_name,
+                        cbmc_results[dirpath]
+                    )
+                ))
+
+        return function_results, cbmc_results
+
+    @classmethod
+    def new(cls,name:str,total_functions:int):
+        function_results_dict, cbmc_results_dict = cls.load_from_csv_files(name)
+
+        return cls(name=name,
+                total_functions=total_functions,
+                cbmc_results_dict=cbmc_results_dict,
+                function_results_dict=function_results_dict
+        )
+
+    def info(self):
+        print_stage(self.name)
+        changed_percent =\
+            round(self.nr_of_changed_functions()/self.total_functions,4)
+        identity_percent =\
+            round(self.passed_identity_cnt()/self.nr_of_changed_functions(),4)
+        print(f"Changed functions: "
+                f"{self.nr_of_changed_functions()}/{self.total_functions} ({changed_percent})")
+        print("Passed identity analysis: "
+                f"{self.passed_identity_cnt()}/{self.nr_of_changed_functions()} "
+                f"({identity_percent})"
+        )
+        print("Result distribution (pre-analysis):")
+        pprint(self.analysis_dist(ident=True,filter_zero=True))
+        print("Result distribution (full-analysis):")
+        pprint(self.analysis_dist(ident=False,filter_zero=True))
+
+
+    def nr_of_changed_functions(self) -> int:
+        ''' The total number of analyzed functions corresponds to the number of
+        functions that were observed as changed in at least one case,
+        every changed function will generate a CBMC entry even if 
+        it cannot be analyzed in `valid_preconds()` '''
+        return len(self.function_results())
+
+    def passed_identity_cnt(self) -> int:
+        '''
+        Out of the functions that were analyzed, how many passed
+        the identity comparision 
+
+        Note: We know that the total number of full analyses that were
+        performed will be equal to the number of successful ID comparisons
+        '''
+        funcs_with_at_least_one_valid_id_cmp = list(filter(lambda v:
+                AnalysisResult.SUCCESS in v.results_id or
+                AnalysisResult.SUCCESS_UNWIND_FAIL in v.results_id,
+                self.function_results()
+        ))
+        return len(funcs_with_at_least_one_valid_id_cmp)
+
+    def analysis_dist(self,
+     ident:bool,
+     filter_zero:bool=False,
+     combine_unwinds:bool=False,
+     ) -> dict[AnalysisResult,float]:
+        ''' Returns a dict of percentages for each AnalysisResult
+        across every function analysis (during either the full or ID stage). 
+        The list starts with the first entry in the AnalysisResult enum '''
+        if ident:
+            # Identity analysis was performed for every function 
+            # in function_results
+            analysis_steps_performed = sum(map(lambda f: len(f.results_id),
+                self.function_results()))
+        else:
+            # Functions which did not pass the identity check will have len()==0
+            # of their results array.
+            analysis_steps_performed = sum(map(lambda f: len(f.results),
+                self.function_results()))
+
+        filtered_cbmc_results = filter(lambda c: c.identity == ident,
+                self.cbmc_results())
+
+        result_cnts = { e.name: 0 for e in AnalysisResult }
+        for cbmc_result in filtered_cbmc_results:
+            result_cnts[cbmc_result.result.name] += 1
+
+        if combine_unwinds:
+            result_cnts[AnalysisResult.SUCCESS.name] += \
+                result_cnts[AnalysisResult.SUCCESS_UNWIND_FAIL.name]
+            result_cnts[AnalysisResult.FAILURE.name] += \
+                result_cnts[AnalysisResult.FAILURE_UNWIND_FAIL.name]
+            del result_cnts[AnalysisResult.SUCCESS_UNWIND_FAIL.name]
+            del result_cnts[AnalysisResult.FAILURE_UNWIND_FAIL.name]
+
+        if filter_zero:
+            result_cnts = { key: val
+                    for key,val in result_cnts.items() if val != 0
+            }
+
+        analysis_dict = { AnalysisResult[tpl[0]]: tpl[1]/analysis_steps_performed
+                for tpl in result_cnts.items() }
+
+        assert sum([ x for x in analysis_dict.values() ]) == 1
+        return analysis_dict
+
 
