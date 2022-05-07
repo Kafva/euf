3182465ef514a06d6a168e7e149d286bfdb152bb
Kafva <Martensson.Jonas98@gmail.com>
2022-05-07 10:52:54 +0200

    -D configurations for libusb

--- scripts/random_wrapper.sh
+++ scripts/random_wrapper.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # Run with `time`!
-CASE_CNT=10
-export TIMEOUT=-1
+CASE_CNT=30
+export TIMEOUT=60
 export VERBOSITY=-1
 export BATCH=true
 
 for _ in $(seq $CASE_CNT); do
 
-  #./scripts/random_case.sh libonig
-  #./scripts/run_without_cbmc.sh $(cat /tmp/path_to_prev_random_case)
+  ./scripts/random_case.sh libonig
+  ./scripts/run_without_cbmc.sh $(cat /tmp/path_to_prev_random_case)
 
   ./scripts/random_case.sh libexpat
   ./scripts/run_without_cbmc.sh $(cat /tmp/path_to_prev_random_case)
