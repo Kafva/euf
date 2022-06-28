3ff0a1f6f73245814420d38591ddfe59260d280f
Kafva <Martensson.Jonas98@gmail.com>
2022-06-28 09:26:35 +0200

    Update examples

--- tests/drivers/example2.c
+++ tests/drivers/example2.c
@@ -1,9 +1,12 @@
 int nondet_int();
+int sum_old(int a, int b){ return a+b; }
 int sum(int a, int b){ return a-b; /*???*/ }
 
 void euf_main(){
   int x = nondet_int();
   int y = nondet_int();
-  __CPROVER_assert(sum(x,y)==x+y, "Expected behaviour");
+  int ret_old = sum_old(x,y);
+  int ret = sum(x,y);
+  __CPROVER_assert(ret_old == ret, "Equivalent output");
 }
 
